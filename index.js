d0a4114925bdb5b8580f9113b68064a2563a2143
Nebras Damsarkho <nebras4u@gmail.com>
2023-04-12 13:28:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var client  = mqtt.connect('mqtt://broker.hivemq.com:1883')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://nebras4db:ab094724385@cluster0.wxkxein.mongodb.net/?retryWrites=true&w=majority";
 
 client.on('connect', function () {
     client.subscribe('presence2121') ;
@@ -14,12 +16,18 @@ client.on('connect', function () {
 client.on('message', function (topic,message) {
     //console.log(message.toString());
 	var toInt = parseInt(message);
-	if(toInt!=""){io.emit('slider1', toInt);
-					io.emit('slider2', toInt);
-					io.emit('slider3', toInt);
-					}
+	if(toInt!="-1"){io.emit('slider1', toInt);
+				      	io.emit('slider2', toInt);
+                io.emit('slider3', toInt);
+          //
+          MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("dbname");
+              dbo.collection("cats").insertOne({date_: new Date(), value: toInt }); //creat collection
+            });
+            //
+          }      
 });
-
 app.use('/assets', express.static('assets'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -39,7 +47,6 @@ io.on('connection', function(socket){
     if(msg!="")//io.emit('slider3', msg);
     client.publish('presence2121',""+msg);
   });
-  
 });
 http.listen(port, function(){
   console.log('listening on *:' + port);
